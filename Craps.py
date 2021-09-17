c01d8c0b0a08add91be9722d20850109711a9287
IPOleksenko <69796661+IPOleksenko@users.noreply.github.com>
2021-09-17 23:54:21 +0300

    Update Craps.py
    Bug fix

--- Craps.py
+++ Craps.py
@@ -1,65 +1,59 @@
 import random
+m= 0
+n= 0
+b= 0
+wi='''
+Победа: {}
+Пройгрыш {}
+Нечья: {}
+'''
+Ru= ('Русанус')
+Ka=('Каха')
+Re=('Резус')
+name2P= (Ru, Ka, Re)
 def start(): 
-	global wi
-	global m
-	global n
-	global b
-	m= ('0')
-	n= ('0')
-	b= ('0')
-	wi='''
-	Победа: {}
-	Пройгрыш {}
-	Нечья: {}
-	'''
-	name1P= input('Твоё имя\n>>')
-	Ru= ('Русанус')
-	Ka=('Каха')
-	Re=('Резус')
-	name2P= (Ru, Ka, Re)
-	P2= (random.choice(name2P))
-	a=input('Привет,'+ name1P+',это игра в кости\nДля того чтобы начать игру напиши "1".\nДля того что бы выйти напиши "0"\n>>')
-	if a == '1':
-		print('Имя противника- ' + P2)
-		act2(name1P, P2)
-	elif a == '0':
-		exit()
-	else:
-		start() 
+    global name1P
+    name1P= input('Твоё имя\n>>')
+    global P2
+    P2= (random.choice(name2P))
+    a=input('Привет,'+ name1P+',это игра в кости\nДля того чтобы начать игру напиши "1".\nДля того что бы выйти напиши "0"\n>>')
+    if a == '1':
+        print('Имя противника- ' + P2)
+        act2()
+    elif a == '0':
+        exit()
+    else:
+        start() 
 		
-def act2(name1P, P2):
-	p= (random.randint(1, 6))
-	o= (random.randint(1, 6))
-	print(name1P, '-' ,p)
-	print(P2, '-', o)
-
-
-	if p > o :
-	 	print('Ты выиграл')
-	 	m=+ 1
-	elif p < o :
-	 	print('Ты проиграл')
-	 	n=+ 1
-	elif p == o :
-	 	print('Нечья')
-	 	b=+ 1
-	end()
+def act2():
+    p= (random.randint(1, 6))
+    o= (random.randint(1, 6))
+    print(name1P, '-' ,p)
+    print(P2, '-', o)
+    if p > o:
+        print('Ты выиграл')
+        global m
+        m = (m + 1)
+    elif p < o:
+        print('Ты проиграл')
+        global n
+        n = 1+n
+    elif p == o:
+        print('Нечья')
+        global b
+        b = 1+b
+    end()
 def end():
-	w=input('Для ещё одного раунда напиши "1",посмотреть результаты напиши "2", чтобы выйти напиши "0"')
-	if w== '1':
-		act2()
-	elif w=='0':
-		exit()
-	elif w =='2':
-		win()
-	else:
-		end()
+    w=input('Для ещё одного раунда напиши "1",посмотреть результаты напиши "2", чтобы выйти напиши "0">>')
+    if w== '1':
+    	act2()
+    elif w=='0':
+        exit()
+    elif w =='2':
+        win()
+    else:
+        end()
 	
 def win():
-
-
-	print(wi.format(m, n, b))
-	
-if __name__=='__main__':
-	while True:
-		start()
+    print(wi.format(m, n, b))
+start()
